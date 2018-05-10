482c5de4911057fab9d700c3cbf83f83199041b6
Benjamin Scabbia <benscabbia@gmail.com>
2018-05-10 07:12:18 +0100

    Added NamedTheoryAttribute

---
+++ AutoName.xUnit/NamedTheoryAttribute.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit.Sdk;
+
+namespace AutoName.xUnit
+{
+	[XunitTestCaseDiscoverer("Xunit.Sdk.TheoryDiscoverer", "xunit.execution.{Platform}")]
+	public class NamedTheoryAttribute : NamedFactAttribute
+    {
+		public NamedTheoryAttribute(NameIt nameIt, SplitBy splitBy, JoinWith joinWith, [CallerMemberName] string callerName = null, [CallerFilePath] string sourceFilePath = null)
+		: base(nameIt, splitBy, joinWith, callerName, sourceFilePath)
+		{
+		}
+
+		public NamedTheoryAttribute(SplitBy splitBy, JoinWith joinWith, [CallerMemberName] string callerName = null, [CallerFilePath] string sourceFilePath = null)
+		: this(NameIt.MethodName, splitBy, joinWith, callerName, sourceFilePath)
+		{ }
+
+
+		public NamedTheoryAttribute([CallerMemberName] string callerName = null, [CallerFilePath] string sourceFilePath = null)
+		: this(NameIt.MethodName, SplitBy.Uppercase, JoinWith.SingleSpace, callerName, sourceFilePath)
+		{ }
+	}
+}
